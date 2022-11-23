dce939b388d67889704cb66ae0dbf1da61e58fbe
memxri <116702671+memxri@users.noreply.github.com>
2022-11-23 12:13:39 +0700

    Add files via upload

---
+++ List_element.py G.8 2.py
@@ -0,0 +1,8 @@
+goals = ['call to grandma', 'shopping', 'cock dinner']
+print(goals)
+i = int(input('Which goal do you want to change? '))
+goals[i] = input("enter a new goal: ")
+print(goals)
+#i = input('Which goal do you want to print?: ')
+#i = int(i) # convert letter(str) to the number (integer)
+#print(goals[i])
