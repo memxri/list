c0b73210204dc52edc377b281e364878e3b17948
memxri <116702671+memxri@users.noreply.github.com>
2022-12-21 12:08:57 +0700

    Create color.py

---
+++ color.py
@@ -0,0 +1,10 @@
+colors = []
+
+# Use a while loop to append colors to the list
+while len(colors) < 5:
+    color = input("Enter a color: ")
+    colors.append(color)
+
+# Use a for loop to print each element in the list
+for color in colors:
+    print(color)
